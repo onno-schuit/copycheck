99f30c64f7d03821022a8ae28ac8b801a5a2d461
Martijn Spruijt <martijn@solin.nl>
2015-01-22 14:39:41 +0100

    Update copycheck

--- moodle-plagiarism_copycheck/version.php
+++ moodle-plagiarism_copycheck/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015011900;
+$plugin->version   = 2015012200;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'plagiarism_copycheck';
